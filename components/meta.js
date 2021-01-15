11c21455fe26ffbb97df5573ae58669a042f2a34
Tom Clare <tclare95@gmail.com>
2021-01-15 23:31:18 +0000

    fixed meta name bug

--- components/meta.js
+++ components/meta.js
@@ -5,7 +5,7 @@ export default function Meta () {
     return (
         <Head>
             <title>Is HPP Open?</title>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
             <script data-ad-client="ca-pub-3944736333183086" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <meta

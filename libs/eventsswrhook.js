f27d2f27ff9652d559ce7296e7c0ec179483674e
Tom Clare <tclare95@gmail.com>
2021-01-16 22:19:24 +0000

    fixed tupo in event hook

--- libs/eventsswrhook.js
+++ libs/eventsswrhook.js
@@ -3,7 +3,7 @@ import {fetcher} from "./fetcher";
 
 export const useFetchedEvents = () => {
     const { data, error, mutate } = useSWR("/api/events", fetcher); 
-  
+    //checking for presence of data
     const eventData = data
     const isPending = !data
   

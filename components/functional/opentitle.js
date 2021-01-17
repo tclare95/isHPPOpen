60d870141a749e4758b50ce9ae45dc3295b0f3ca
Tom Clare <tclare95@gmail.com>
2021-01-17 13:41:35 +0000

    fixed unexpected error on forEach eventData bug

--- components/functional/opentitle.js
+++ components/functional/opentitle.js
@@ -15,7 +15,7 @@ export default function OpenTitle () {
         )
     }
     
-    if (isPending || eventIsPending) {
+    if (isPending || eventIsPending || !eventData) {
         return (
             <div>
                 Loading

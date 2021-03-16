f0c2531bae5c2422ca306f6312c8c2ff1f302f9c
Tom Clare <tclare95@gmail.com>
2021-03-16 15:33:03 +0000

    Updated Reopening

--- components/layout/frontpage/header.js
+++ components/layout/frontpage/header.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 export default function Header () {
     return (
         <Row className="justify-content-center">
-            <Alert variant="danger">HPP is closed until further notice. EA forecast data has an issue and is not lining up.</Alert>
+            <Alert variant="danger">HPP will reopen 29/03/21. EA forecast data has an issue and is not lining up.</Alert>
         </Row>
     )
 }

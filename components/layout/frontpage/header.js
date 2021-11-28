b3b4b40b934f0365fee0fb487686cf0dbf95a668
Tom Clare <tclare95@gmail.com>
2021-11-28 23:47:09 +0000

    Added direct booking links

--- components/layout/frontpage/header.js
+++ components/layout/frontpage/header.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 export default function Header () {
     return (
         <Row className="justify-content-center">
-            <Alert variant="danger">Environment Agency Level forecast is re-enabled.</Alert>
+            <Alert variant="danger">Environment Agency Level forecast is re-enabled and direct session booking links are now working</Alert>
         </Row>
     )
 }

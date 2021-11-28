25a8e9cd25b594d876ed50d266d9c07989d0a8f6
Tom Clare <tclare95@gmail.com>
2021-11-28 23:43:59 +0000

    Updated Banner Message

--- components/layout/frontpage/header.js
+++ components/layout/frontpage/header.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 export default function Header () {
     return (
         <Row className="justify-content-center">
-            <Alert variant="danger">Environment Agency Level forecast is broken, so is not being displayed at this time.</Alert>
+            <Alert variant="danger">Environment Agency Level forecast is re-enabled.</Alert>
         </Row>
     )
 }

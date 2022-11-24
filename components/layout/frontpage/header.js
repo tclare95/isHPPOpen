b5fbfba7649b83c11331beab420ae65ef5b88d3c
Tom Clare <tclare95@gmail.com>
2022-11-24 18:17:58 +0000

    Updated nextjs version

--- components/layout/frontpage/header.js
+++ components/layout/frontpage/header.js
@@ -4,12 +4,10 @@ import Row from 'react-bootstrap/Row'
 
 export default function Header ({message}) {
     return (
-        <Link href="/worlds">
+        
         
         <Row className="justify-content-center mx-5 mt-1">
             <Alert variant="danger text-center">{message}</Alert>
         </Row>
-
-        </Link>
     )
 }

48ec5edbf83053b881d8a4695b5c19cf1602052b
Tom Clare <tclare95@gmail.com>
2022-11-25 22:52:13 +0000

    added trent lock monitoring functionality

---
+++ pages/api/trentlockapi.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { connectToDatabase } from "../../libs/database";
+
+const getLevelByStation = async (stationId, dateObject) => {
+  //takes a station id and a date object and returns the level at the station for the preceding 2 days.
+  // returns an array of objects with the date and level
+  let arrayToReturn = [];
+  const dayZero = dateObject.toISOString().split("T")[0];
+//   const dayOne = new Date(dateObject.setDate(dateObject.getDate() - 1))
+//     .toISOString()
+//     .split("T")[0];
+
+  const levelZero = await axios.get(
+    `https://environment.data.gov.uk/flood-monitoring/id/stations/${stationId}/readings?_sorted&date=${dayZero}`
+  );
+//   const levelOne = await axios.get(
+//     `https://environment.data.gov.uk/flood-monitoring/id/stations/${stationId}/readings?_sorted&date=${dayOne}`
+//   );
+  levelZero.data.items.forEach((item) =>
+    arrayToReturn.push({ date: item.dateTime, level: item.value })
+  );
+//   levelOne.data.items.forEach((item) =>
+//     arrayToReturn.push({ date: item.dateTime, level: item.value })
+//   );
+
+  return arrayToReturn;
+};
+
+module.exports = async (req, res) => {
+  const { method, body } = req;
+  let submissionDate;
+  // turn the request into a JSON object
+
+  // try parsing the body, if it fails, return an error
+  try {
+    const request = JSON.parse(Object.keys(body)[0]);
+    console.log(request);
+    submissionDate = new Date(request.dateTime);
+    res.status(200).json({
+      message: "Update successful",
+    });
+  } catch (error) {
+    res.status(403).json({
+      message: "Error parsing request body",
+    });
+  }
+  // this stuff is all done asynchronously, so we can't return a response here
+  // access the EA API, pull data for colwick (#12345), clifton (#12346), shardlow(#123456), church wilne (#1234567), and kegworth (#12345678) for the previous 7 days.
+  // also pull the rainfall api data for ashford hall, cresswell, sutton coldfield and wanlip for the previous 7 days.
+  // return the data as a JSON object
+  const levelStations = {
+    colwick: 4009,
+    clifton: 2217,
+    shardlow: 2100,
+    churchWilne: 2130,
+    kegworth: 2132,
+  };
+
+  const rainfallStations = {
+    ashfordHall: 123456789,
+    cresswell: 1234567890,
+    suttonColdfield: 12345678901,
+    wanlip: 123456789012,
+  };
+  let levelData = {};
+  let rainfallData = {};
+
+  try {
+    const request = JSON.parse(Object.keys(body)[0]);
+    Object.keys(levelStations).forEach((station) => {
+      console.log(station);
+      console.log(levelStations[station]);
+      levelData[station] = getLevelByStation(
+        levelStations[station],
+        submissionDate
+      );
+    });
+
+    let levels = await Promise.all(Object.values(levelData));
+    console.log(levels);
+    // add everything to the database
+    const documentToInsert = {
+        date: request.dateTime,
+        dateCreated: new Date(),
+        userRange: request.range,
+        userComment: request.comment,
+        userBoat: request.boat,
+        recordedLevels: levels
+    }
+
+    const { db } = await connectToDatabase();
+    const collection = db.collection("trentlockdata");
+    const result = await collection.insertOne(documentToInsert);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
---
+++ pages/trentlock.js
@@ -0,0 +1,156 @@
+import { Formik } from "formik";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Header from "../components/layout/frontpage/header";
+import Meta from "../components/meta";
+import Link from "next/link";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
+import axios from "axios";
+
+const validate = (values) => {
+    const errors = {};
+    if (!values.dateTime) {
+        errors.dateTime = "Required";
+    }
+    if (!values.range) {
+        errors.range = "Required";
+    }
+    
+    return errors;
+};
+
+export default function TrentLock() {
+    
+  const now = new Date();
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  return (
+    <Container fluid className="bg-dark">
+      <Meta title="Trent Lock Info Gathering - Is HPP Open" />
+      <Header message="This will gather information about trent lock levels to produce a better forecast model" />
+      <Container className="text-white text-center">
+        <Row className="mb-5">
+          <Col>
+            <h1>Trent Lock Levels</h1>
+            <Link href="/">
+              <h3 className="navHeader">← Back to Main Page</h3>
+            </Link>
+            <p>
+              This page will gather information about trent lock levels to
+              produce a better forecast model. This is a work in progress and
+              will be updated as more information is gathered.
+            </p>
+            <p>
+              If you paddle at Trent lock, please consider filling out this form
+              to help us improve our forecast model. I'll correlate the data of
+              when you paddle with the river levels to produce a better forecast
+              model.
+            </p>
+            <p>You can also add comments with more info if you want</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h4>Enter your info here</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Formik
+            validate={validate}
+             initialValues={{
+                range: 3,
+             }}
+              onSubmit={(values, { setSubmitting, resetForm }) => {
+                console.log(values);
+                const dataArray = {
+                  dateTime: values.dateTime,
+                  range: values.range,
+                  comments: values.comments ? values.comments : "No comment",
+                  boatType: values.boatType? values.boatType : "No boat type",
+                };
+                const json = JSON.stringify(dataArray);
+                console.log(json);
+                axios
+                  .post("/api/trentlockapi", json)
+                  .then((response) => {
+                    if (response.status === 200) {
+                      setSuccess(true);
+                    } else {
+                      setError(true);
+                    }
+                  });
+                // don't reset the form - in case they want to add another event
+                // resetForm();
+              }}
+            >
+              {(formik) => (
+                <Form onSubmit={formik.handleSubmit}>
+                  <Form.Group className="mb-3">
+                    <Form.Label htmlFor="dateTime">
+                      Date / Time of paddle
+                    </Form.Label>
+                    <Form.Control
+                      name="dateTime"
+                      type="datetime-local"
+                      id="dateTime"
+                      onChange={formik.handleChange}
+                    ></Form.Control>
+                    {formik.errors.dateTime ? (<p className="text-danger">please fill in the date / time - it doesn't need to be exact!</p>): null}
+                    <Form.Label htmlFor="rating">
+                      Rating out of 6 (1 = not paddleable, 5 = amazing)
+                    </Form.Label>
+                    <Form.Range
+                      min="1"
+                      max="5"
+                      id="range"
+                      onChange={formik.handleChange}
+                      name="range"
+                    />
+                    {formik.errors.range ? (<p className="text-danger">please fill in the range!</p>) : null}
+                    <p>{formik.values.range}</p>
+                    <Form.Label htmlFor="comments">
+                      Any comments on the level (optional)
+                    </Form.Label>
+                    <Form.Control
+                      id="comments"
+                      type="text"
+                      placeholder="Comments"
+                      onChange={formik.handleChange}
+                      name="comments"
+                    />
+                    <Form.Label htmlFor="boatType">
+                      What kind of boat? (optional)
+                    </Form.Label>
+                    <Form.Select
+                      id="boatType"
+                      onChange={formik.handleChange}
+                      name="boatType"
+                    >
+                      <option>playboat</option>
+                      <option>longer boat</option>
+                      <option>surf boat</option>
+                    </Form.Select>
+
+                    <Button variant="primary" type="submit">
+                      Submit
+                    </Button>
+                  </Form.Group>
+                </Form>
+              )}
+            </Formik>
+            {success && (
+              <div>
+                <h5>Log successfully added!</h5>
+              </div>
+            )}
+            {error && <div>Error adding log!</div>}
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
